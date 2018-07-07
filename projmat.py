66fd6c3ad6c9c723f1c6fb9b5deef904bd84b629
Diogo R. Ferreira <diogo.ribeiro.ferreira@gmail.com>
2018-07-07 18:06:51 +0100

    added projmat.py

---
+++ projmat.py
@@ -0,0 +1,111 @@
+from __future__ import print_function
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from shapely.geometry import MultiLineString, LineString
+
+# -----------------------------------------------------------------------------------------
+
+fname = 'cameras.csv'
+print('Reading:', fname)
+df = pd.read_csv(fname)
+
+print(df)
+
+# -----------------------------------------------------------------------------------------
+
+n_rows = 15  # y-axis pixel resolution
+n_cols = 15  # x-axis pixel resolution
+
+x_min = -100.
+x_max = +100.
+
+y_min = -100.
+y_max = +100.
+
+def transform(x, y):
+    j = int((x-x_min)/(x_max-x_min)*n_cols)
+    i = int((y_max-y)/(y_max-y_min)*n_rows)
+    return (i, j)
+
+# -------------------------------------------------------------------------
+
+x_grid = np.linspace(x_min, x_max, num=n_cols+1)
+y_grid = np.linspace(y_min, y_max, num=n_rows+1)
+
+grid = []
+
+for x in x_grid:
+    grid.append([(x, y_min), (x, y_max)])
+
+for y in y_grid:
+    grid.append([(x_min, y), (x_max, y)])
+
+grid = MultiLineString(grid)
+
+masks = []
+
+for row in df.itertuples():
+    line = LineString([(row.x0, row.y0), (row.x1, row.y1)])
+    mask = np.zeros((n_rows, n_cols))
+    for segment in line.difference(grid):
+        xx, yy = segment.xy
+        x_mean = np.mean(xx)
+        y_mean = np.mean(yy)
+        (i, j) = transform(x_mean, y_mean)
+        mask[i,j] = segment.length
+    masks.append(mask)
+    
+masks = np.array(masks)
+
+print('masks:', masks.shape, masks.dtype)
+
+# -------------------------------------------------------------------------
+
+vmin = 0.
+vmax = np.sqrt(((x_max-x_min)/n_cols)**2 + ((y_max-y_min)/n_rows)**2)
+
+ni = 2
+nj = 4
+figsize = (2*nj, 2*ni)
+
+fig, ax = plt.subplots(ni, nj, figsize=figsize)
+for i in range(ni):
+    for j in range(nj):
+        k = i*nj + j
+        ax[i,j].imshow(masks[k], vmin=vmin, vmax=vmax)
+        ax[i,j].set_axis_off()
+
+fig.suptitle('projection matrix (top camera)')
+plt.show()
+
+fig, ax = plt.subplots(ni, nj, figsize=figsize)
+for i in range(ni):
+    for j in range(nj):
+        k = i*nj + j + ni*nj
+        ax[i,j].imshow(masks[k], vmin=vmin, vmax=vmax)
+        ax[i,j].set_axis_off()
+
+fig.suptitle('projection matrix (front camera)')
+plt.show()
+
+fig, ax = plt.subplots(ni, nj, figsize=figsize)
+for i in range(ni):
+    for j in range(nj):
+        k = i*nj + j + 2*ni*nj
+        ax[i,j].imshow(masks[k], vmin=vmin, vmax=vmax)
+        ax[i,j].set_axis_off()
+
+fig.suptitle('projection matrix (bottom camera)')
+plt.show()
+
+# -------------------------------------------------------------------------
+
+projmat = masks.reshape((masks.shape[0], masks.shape[1]*masks.shape[2]))
+
+print('projmat:', projmat.shape, projmat.dtype)
+
+fname = 'projmat.npy'
+print('Writing:', fname)
+np.save(fname, projmat)
