ff854365661e11ad4ac0d7331074b97e9b41064c
Diogo R. Ferreira <diogo.ribeiro.ferreira@gmail.com>
2018-07-08 10:06:46 +0100

    added signals.py

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ The code is based on the 3-camera setup described in the [PhD thesis](http://bib
 
 For a different setup, feel free to adapt this code according to the license terms.
 
-## Instructions
+## Cameras
 
 - Run `cameras.py` to find the lines of sight for each camera.
 
@@ -21,6 +21,8 @@ For a different setup, feel free to adapt this code according to the license ter
     - An output file `cameras.csv` will be created with the start and end positions for each line of sight.
     
 ![cameras](https://raw.githubusercontent.com/diogoff/isttok-tomography/master/images/cameras.png)
+
+## Projections
 
 - Run `projections.py` to find the projections from pixel values to detector measurements.
 
@@ -35,3 +37,23 @@ For a different setup, feel free to adapt this code according to the license ter
 ![projections-top](https://raw.githubusercontent.com/diogoff/isttok-tomography/master/images/projections-top.png)
 ![projections-front](https://raw.githubusercontent.com/diogoff/isttok-tomography/master/images/projections-front.png)
 ![projections-bottom](https://raw.githubusercontent.com/diogoff/isttok-tomography/master/images/projections-bottom.png)
+
+## Signals
+
+- Run `signals.py` to get the signals from each camera, for a given shot number.
+
+    - The code uses the SDAS API that should be downloaded and installed from [here](http://metis.ipfn.ist.utl.pt/CODAC/IPFN_Software/SDAS/Access/Python).
+    
+    - The shot number is indicated in the code.
+    
+    - The data acquisition channels that correspond to each detector are also indicated in the code.
+    
+    - The signal offset is removed based on the signal average for _t_ < 0 s.
+    
+    - The signals are subsampled from 2 MHz to 10 kHz.
+    
+    - The signals data and time are stored separately in `signals_data.npy` and `signals_time.npy`.
+    
+![signals-top](https://raw.githubusercontent.com/diogoff/isttok-tomography/master/images/signals-top.png)
+![signals-front](https://raw.githubusercontent.com/diogoff/isttok-tomography/master/images/signals-front.png)
+![signals-bottom](https://raw.githubusercontent.com/diogoff/isttok-tomography/master/images/signals-bottom.png)
---
+++ signals.py
@@ -0,0 +1,117 @@
+from __future__ import print_function
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# -------------------------------------------------------------------------
+# http://metis.ipfn.ist.utl.pt/CODAC/IPFN_Software/SDAS/Codes/Python
+
+from sdas.core.client.SDASClient import SDASClient
+from sdas.core.SDAStime import TimeStamp
+
+def StartSdas():
+    host = 'baco.ipfn.tecnico.ulisboa.pt'
+    port = 8888
+    client = SDASClient(host, port)
+    return client
+
+def LoadSdasData(client, channelID, shotnr):
+    dataStruct = client.getData(channelID, '0x0000', shotnr)
+    dataArray = dataStruct[0].getData()
+    len_d = len(dataArray)
+    tstart = dataStruct[0].getTStart()
+    tend = dataStruct[0].getTEnd()
+    tbs= (tend.getTimeInMicros() - tstart.getTimeInMicros())*1.0/len_d
+    events = dataStruct[0].get('events')[0]
+    tevent = TimeStamp(tstamp=events.get('tstamp'))
+    delay = tstart.getTimeInMicros() - tevent.getTimeInMicros()
+    timeVector = np.linspace(delay,delay+tbs*(len_d-1),len_d)
+    return [dataArray, timeVector]
+
+# -------------------------------------------------------------------------
+
+client = StartSdas()
+
+shot = 17552
+
+channels = [
+    # top camera
+    'TR512_TOMOGRAPHY.TR512_B02.CHANNEL_0',
+    'TR512_TOMOGRAPHY.TR512_B02.CHANNEL_1',
+    'TR512_TOMOGRAPHY.TR512_B02.CHANNEL_2',
+    'TR512_TOMOGRAPHY.TR512_B02.CHANNEL_3',
+    'TR512_TOMOGRAPHY.TR512_B02.CHANNEL_4',
+    'TR512_TOMOGRAPHY.TR512_B02.CHANNEL_5',
+    'TR512_TOMOGRAPHY.TR512_B02.CHANNEL_6',
+    'TR512_TOMOGRAPHY.TR512_B02.CHANNEL_7',
+    # front camera
+    'TR512_TOMOGRAPHY.TR512_B00.CHANNEL_0',
+    'TR512_TOMOGRAPHY.TR512_B00.CHANNEL_1',
+    'TR512_TOMOGRAPHY.TR512_B00.CHANNEL_2',
+    'TR512_TOMOGRAPHY.TR512_B00.CHANNEL_3',
+    'TR512_TOMOGRAPHY.TR512_B00.CHANNEL_4',
+    'TR512_TOMOGRAPHY.TR512_B00.CHANNEL_5',
+    'TR512_TOMOGRAPHY.TR512_B00.CHANNEL_6',
+    'TR512_TOMOGRAPHY.TR512_B00.CHANNEL_7',
+    # bottom camera
+    'TR512_TOMOGRAPHY.TR512_B01.CHANNEL_0',
+    'TR512_TOMOGRAPHY.TR512_B01.CHANNEL_1',
+    'TR512_TOMOGRAPHY.TR512_B01.CHANNEL_2',
+    'TR512_TOMOGRAPHY.TR512_B01.CHANNEL_3',
+    'TR512_TOMOGRAPHY.TR512_B01.CHANNEL_4',
+    'TR512_TOMOGRAPHY.TR512_B01.CHANNEL_5',
+    'TR512_TOMOGRAPHY.TR512_B01.CHANNEL_6',
+    'TR512_TOMOGRAPHY.TR512_B01.CHANNEL_7']
+
+signals_data = []
+signals_time = []
+
+for channel in channels:
+    print('channel:', channel)
+    [data, time] = LoadSdasData(client, channel, shot)
+    i0 = np.argmin(np.fabs(time))
+    data -= np.mean(data[:i0])
+    time *= 1e-6
+    n = 200
+    data = np.cumsum(data, axis=0)
+    data = (data[n:]-data[:-n])/n
+    data = data[::n]
+    data = np.clip(data, 0., None)
+    time = time[n//2::n]
+    time = time[:data.shape[0]]
+    signals_data.append(data)
+    signals_time.append(time)
+    plt.plot(time, data)
+    if channel == channels[7]:
+        plt.title('signals (top camera)')
+        plt.xlabel('t (s)')
+        plt.legend()
+        plt.show()
+    if channel == channels[15]:
+        plt.title('signals (front camera)')
+        plt.xlabel('t (s)')
+        plt.legend()
+        plt.show()
+    if channel == channels[23]:
+        plt.title('signals (bottom camera)')
+        plt.xlabel('t (s)')
+        plt.legend()
+        plt.show()
+
+# -------------------------------------------------------------------------
+
+signals_data = np.array(signals_data)
+signals_time = np.array(signals_time)
+
+print('signals_data:', signals_data.shape, signals_data.dtype)
+print('signals_time:', signals_time.shape, signals_time.dtype)
+
+# -------------------------------------------------------------------------
+
+fname = 'signals_data.npy'
+print('Writing:', fname)
+np.save(fname, signals_data)
+
+fname = 'signals_time.npy'
+print('Writing:', fname)
+np.save(fname, signals_time)
