69620bb486c19fe82695cc7d2e9e0808cb0be1b1
Diogo R. Ferreira <diogo.ribeiro.ferreira@gmail.com>
2019-07-18 15:34:25 +0100

    changed cameras.py

--- cameras.py
+++ cameras.py
@@ -7,122 +7,86 @@ from shapely.geometry import Point, LineString
 
 # -----------------------------------------------------------------------------------------
 
-t_pinhole_x = 5.
-t_pinhole_y = 97.
+v_pinhole_x = 5.
+v_pinhole_y = 97.
 
-f_pinhole_x = 109.
-f_pinhole_y = 0.
+h_pinhole_x = 109.
+h_pinhole_y = 0.
 
-# b_pinhole_x = 5. + 102 * np.cos(np.radians(-82.5))    ***UNCOMMENT TO ADD BOTTOM CAMERA***
-# b_pinhole_y = 102.0 * np.sin(np.radians(-82.5))
+print('v_pinhole_x:', v_pinhole_x)
+print('v_pinhole_y:', v_pinhole_y)
 
-print('t_pinhole_x:', t_pinhole_x)
-print('t_pinhole_y:', t_pinhole_y)
-
-print('f_pinhole_x:', f_pinhole_x)
-print('f_pinhole_y:', f_pinhole_y)
-
-# print('b_pinhole_x:', b_pinhole_x)    ***UNCOMMENT TO ADD BOTTOM CAMERA***
-# print('b_pinhole_y:', b_pinhole_y)
+print('h_pinhole_x:', h_pinhole_x)
+print('h_pinhole_y:', h_pinhole_y)
 
 # -----------------------------------------------------------------------------------------
 
-n = 16              # number of detectors per camera
-size = 0.75         # detector size
-space = 0.2         # space between detectors
-step = size+space   # distance between the centers of adjacent detectors
+n = 16               # number of detectors per camera
+size = 0.75          # detector size
+space = 0.2          # space between detectors
+step = size + space  # distance between the centers of adjacent detectors
 
-t_dist = 9.     # distance from camera to pinhole
-f_dist = 9.
-# b_dist = 13.  ***UNCOMMENT TO ADD BOTTOM CAMERA***
+v_dist = 9.          # distance from camera to pinhole
+h_dist = 9.
 
-t_theta = np.pi                 # Top: rotated 180deg (upside down)
-f_theta = np.pi / 2.            # Front: vertical on the LFS (facing left) 90deg
-# b_theta = np.pi * 7.5 / 180.    # Bottom: looking up at slight positive angle 7.5deg  ***UNCOMMENT TO ADD BOTTOM CAMERA***
+v_theta = np.pi      # vertical camera: rotated 180 degrees from bottom
+h_theta = np.pi / 2. # horizontal camera: rotated 90 degrees from bottom
 
 # ----------------------------------------------------------------------------------------
 
-##########################################################################################
-#                                   TOP                                                  #
-##########################################################################################
-# Pinhole frame of reference
-t_detector_x = np.linspace(1., -1., n) * step * (n - 1.) / 2.
-t_detector_y = - np.ones(n) * t_dist
+# pinhole frame of reference
+v_detector_x = np.linspace(1., -1., n) * step * (n-1.)/2.
+v_detector_y = (-1.) * np.ones(n) * v_dist
 
-# Rotation and translation to the tokamak frame of reference
-t_detector_rotated_x = t_detector_x*np.cos(t_theta) - t_detector_y*np.sin(t_theta)
-t_detector_rotated_y = t_detector_x*np.sin(t_theta) + t_detector_y*np.cos(t_theta)
-t_detector_x = t_pinhole_x + t_detector_rotated_x
-t_detector_y = t_pinhole_y + t_detector_rotated_y
+# rotation
+v_detector_rotated_x = v_detector_x*np.cos(v_theta) - v_detector_y*np.sin(v_theta)
+v_detector_rotated_y = v_detector_x*np.sin(v_theta) + v_detector_y*np.cos(v_theta)
 
-# Calibration factors
-t_etendue = [
-    0.03155177, 0.04362264, 0.05633366, 0.06869239,
-    0.09645432, 0.11267896, 0.12971384, 0.14767003,
-    0.15017711, 0.14139539, 0.12288025, 0.10543017,
-    0.08804052, 0.06193492, 0.05265601, 0.05495817,
-]
+# translation to the vessel frame of reference
+v_detector_x = v_pinhole_x + v_detector_rotated_x
+v_detector_y = v_pinhole_y + v_detector_rotated_y
 
-##########################################################################################
-#                                   FRONT                                                #
-##########################################################################################
-# Pinhole frame of reference
-f_detector_x = np.linspace(1., -1., n) * step * (n - 1.) / 2.
-f_detector_y = - np.ones(n) * f_dist
+# calibration factors (etendue)
+v_etendue = [0.03155177, 0.04362264, 0.05633366, 0.06869239,
+             0.09645432, 0.11267896, 0.12971384, 0.14767003,
+             0.15017711, 0.14139539, 0.12288025, 0.10543017,
+             0.08804052, 0.06193492, 0.05265601, 0.05495817]
 
-# Rotation and translation to the tokamak frame of reference
-f_detector_rotated_x = f_detector_x*np.cos(f_theta) - f_detector_y*np.sin(f_theta)
-f_detector_rotated_y = f_detector_x*np.sin(f_theta) + f_detector_y*np.cos(f_theta)
-f_detector_x = f_pinhole_x + f_detector_rotated_x
-f_detector_y = f_pinhole_y + f_detector_rotated_y
+print('v_detector_x:', v_detector_x)
+print('v_detector_y:', v_detector_y)
 
-# Calibration factors
-f_etendue = [
-    0.00210049, 0.00611418, 0.01208648, 0.01871730,
-    0.02721384, 0.03679548, 0.04456807, 0.05030524,
-    0.05118717, 0.04779802, 0.04111613, 0.03216692,
-    0.02290661, 0.01593762, 0.01080871, 0.00883411,
-]
+# ----------------------------------------------------------------------------------------
 
-# ##########################################################################################
-# #                                   BOTTOM                                               #
-# ##########################################################################################
-# # Pinhole frame of reference                                                      ***UNCOMMENT TO ADD BOTTOM CAMERA***
-# b_detector_x = np.linspace(1., -1., n) * step * (n - 1.) / 2.
-# b_detector_y = - np.ones(n) * b_dist
-#
-# # Rotation and translation to the tokamak frame of reference
-# b_detector_rotated_x = b_detector_x*np.cos(b_theta) - b_detector_y*np.sin(b_theta)
-# b_detector_rotated_y = b_detector_x*np.sin(b_theta) + b_detector_y*np.cos(b_theta)
-# b_detector_x = b_pinhole_x + b_detector_rotated_x
-# b_detector_y = b_pinhole_y + b_detector_rotated_y
-#
-# # Calibration factors
-# b_etendue = [
-#     1.0, 1.0, 1.0, 1.0,
-#     1.0, 1.0, 1.0, 1.0,
-#     1.0, 1.0, 1.0, 1.0,
-#     1.0, 1.0, 1.0, 1.0,
-# ]
+# pinhole frame of reference
+h_detector_x = np.linspace(1., -1., n) * step * (n-1.)/2.
+h_detector_y = (-1.) * np.ones(n) * h_dist
 
-print('t_detector_x:', t_detector_x)
-print('t_detector_y:', t_detector_y)
+# rotation
+h_detector_rotated_x = h_detector_x*np.cos(h_theta) - h_detector_y*np.sin(h_theta)
+h_detector_rotated_y = h_detector_x*np.sin(h_theta) + h_detector_y*np.cos(h_theta)
 
-print('f_detector_x:', f_detector_x)
-print('f_detector_y:', f_detector_y)
+# translation to the vessel frame of reference
+h_detector_x = h_pinhole_x + h_detector_rotated_x
+h_detector_y = h_pinhole_y + h_detector_rotated_y
 
-# print('b_detector_x:', b_detector_x)  ***UNCOMMENT TO ADD BOTTOM CAMERA***
-# print('b_detector_y:', b_detector_y)
+# calibration factors (etendue)
+h_etendue = [0.00210049, 0.00611418, 0.01208648, 0.01871730,
+             0.02721384, 0.03679548, 0.04456807, 0.05030524,
+             0.05118717, 0.04779802, 0.04111613, 0.03216692,
+             0.02290661, 0.01593762, 0.01080871, 0.00883411]
+
+print('h_detector_x:', h_detector_x)
+print('h_detector_y:', h_detector_y)
 
 # -----------------------------------------------------------------------------------------
 
 coords = []
 
 for i in range(n):
-    x0 = t_detector_x[i]
-    y0 = t_detector_y[i]
-    x1 = t_pinhole_x
-    y1 = t_pinhole_y
+    x0 = v_detector_x[i]
+    y0 = v_detector_y[i]
+    x1 = v_pinhole_x
+    y1 = v_pinhole_y
     m = (y1-y0)/(x1-x0)
     b = (y0*x1-y1*x0)/(x1-x0)
     y2 = -100.
@@ -132,14 +96,14 @@ for i in range(n):
     segment = line.difference(circle)[1]
     x0, y0 = segment.coords[0]
     x1, y1 = segment.coords[1]
-    print('%10s %10.6f %10.6f %10.6f %10.6f %10.6f' % ('top', x0, y0, x1, y1, t_etendue[i]))
-    coords.append(['top', x0, y0, x1, y1, t_etendue[i]])
+    print('%10s %10.6f %10.6f %10.6f %10.6f %10.6f' % ('top', x0, y0, x1, y1, v_etendue[i]))
+    coords.append(['top', x0, y0, x1, y1, v_etendue[i]])
 
 for i in range(n):
-    x0 = f_detector_x[i]
-    y0 = f_detector_y[i]
-    x1 = f_pinhole_x
-    y1 = f_pinhole_y
+    x0 = h_detector_x[i]
+    y0 = h_detector_y[i]
+    x1 = h_pinhole_x
+    y1 = h_pinhole_y
     m = (y1-y0)/(x1-x0)
     b = (y0*x1-y1*x0)/(x1-x0)
     x2 = -100.
@@ -149,25 +113,8 @@ for i in range(n):
     segment = line.difference(circle)[1]
     x0, y0 = segment.coords[0]
     x1, y1 = segment.coords[1]
-    print('%10s %10.6f %10.6f %10.6f %10.6f %10.6f' % ('front', x0, y0, x1, y1, f_etendue[i]))
-    coords.append(['front', x0, y0, x1, y1, f_etendue[i]])
-
-# for i in range(n):                ***UNCOMMENT TO ADD BOTTOM CAMERA***
-#     x0 = b_detector_x[i]
-#     y0 = b_detector_y[i]
-#     x1 = b_pinhole_x
-#     y1 = b_pinhole_y
-#     m = (y1-y0)/(x1-x0)
-#     b = (y0*x1-y1*x0)/(x1-x0)
-#     y2 = 100.
-#     x2 = (y2-b)/m
-#     line = LineString([(x0, y0), (x2, y2)])
-#     circle = Point(0., 0.).buffer(100.).boundary
-#     segment = line.difference(circle)[1]
-#     x0, y0 = segment.coords[0]
-#     x1, y1 = segment.coords[1]
-#     print('%10s %10.6f %10.6f %10.6f %10.6f % 10.6f' % ('bottom', x0, y0, x1, y1, b_etendue[i]))
-#     coords.append(['bottom', x0, y0, x1, y1, b_etendue[i]])
+    print('%10s %10.6f %10.6f %10.6f %10.6f %10.6f' % ('front', x0, y0, x1, y1, h_etendue[i]))
+    coords.append(['front', x0, y0, x1, y1, h_etendue[i]])
 
 # -----------------------------------------------------------------------------------------
 
